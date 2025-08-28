fe030f370e68037eea124e0f6819906bba9f84c7
Anton Dauhun <anton_dauhun@epam.com>
2025-08-28 19:12:34 +0300

    add table

--- client/src/components/SearchQueries/SearchQuery.tsx
+++ client/src/components/SearchQueries/SearchQuery.tsx
@@ -1,48 +1,19 @@
 import { FC } from "react";
 import { useSelector } from "react-redux";
-import { Collapse, CollapseProps, Table } from "antd";
+import { Collapse, CollapseProps } from "antd";
 
 import { getSearchQueries } from "../../store/wordstat/selectors";
+import { SearchQueryTable } from "./SearchQueryTable/SearchQueryTable";
 
 const SearchQueries: FC = () => {
   const searchQueries = useSelector(getSearchQueries);
 
-  const columns = [
-    {
-      title: "Phrase",
-      key: "phrase",
-      dataIndex: "phrase",
-    },
-    {
-      title: "Count",
-      key: "count",
-      dataIndex: "count",
-      width: 200,
-    },
-  ];
-
   const items: CollapseProps["items"] = Object.values(searchQueries).map(
     (searchQuery) => {
-      const dataSource = searchQuery.queries.map(({ phrase, count, id }) => {
-        return {
-          key: id,
-          phrase,
-          count,
-        };
-      });
-
       return {
         key: searchQuery.id,
         label: searchQuery.phrase,
-        children: (
-          <Table
-            dataSource={dataSource}
-            columns={columns}
-            size="small"
-            pagination={false}
-            scroll={{ y: 500 }}
-          />
-        ),
+        children: <SearchQueryTable data={searchQuery.queries} />,
       };
     }
   );
