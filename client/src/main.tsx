6c07311c55dc4865feb83f6471f741dda80d8820
Anton Dauhun <anton_dauhun@epam.com>
2025-09-11 18:05:38 +0300

    add empty line between import style

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 
 import App from "./App.tsx";
+
 import "./styles/main.css";
 
 createRoot(document.getElementById("root")!).render(
